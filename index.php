b75564ea566744e75c2674294f69ca92288e605d
shotaro <850123.a@gmail.com>
2025-08-13 14:07:30 +0900

    summary_section_pc_complete

--- index.php
+++ index.php
@@ -34,10 +34,10 @@
     </div>
   </section>
   <section class="freelance_section">
-    <div class="main_container freelance">
-      <div class="freelanceTitle_content">
-        <h2 class="freelance_h2">Freelance Summit</h2>
-        <p class="freelance_text"><span class="line_left"></span>フリーランスサミットとは？<span class="line_right"></span></p>
+    <div class="main_container_freelance">
+      <div class="title_content">
+        <h2 class="title_h2">Freelance Summit</h2>
+        <p class="subTitle_text"><span class="line_left"></span>フリーランスサミットとは？<span class="line_right"></span></p>
       </div>
       <div class="summitText_content">
         <p class="summit_text">「FREELANCE SUMMIT」は、StockSun株式会社が主催する日本最大規模のフリーランスイベントです。</p>
@@ -48,9 +48,76 @@
 
       <a class="side_button" href="#">
         <span class="side_button_text">申し込みはこちら
-        <img class="side_line_img" src="./img/LINE_Brand_icon 1 (1).png">
+          <img class="side_line_img" src="./img/LINE_Brand_icon 1 (1).png">
         </span>
       </a>
+
+      <div class="Summary_content">
+        <div class="title_content summary">
+          <h2 class="title_h2">Event Summary</h2>
+          <p class="subTitle_text event"><span class="line_left event"></span>イベント開催概要<span class="line_right event"></span></p>
+        </div>
+        <div class="summaryDetail_content">
+          <p class="summaryDetail_title">開催日程</p>
+          <p class="summaryDetail_detail">
+            <span class="start_text">2023年3月4日（土）</span><span class="kaijou_text">開場10:00</span><span class="end_text">閉場18:00（受付開始9:30-）</span>
+          </p>
+        </div>
+
+        <span class="border_dot"></span>
+
+        <div class="summaryDetail_content kaijou">
+          <p class="summaryDetail_title kaijou">会場</p>
+          <p class="summaryDetail_detail kokusai"><span class="forum_text">東京国際フォーラム</span><span class="holee2_text">ホールE2</span></p>
+        </div>
+
+        <span class="border_dot event"></span>
+
+        <div class="summaryDetail_content event">
+          <p class="summaryDetail_title event">イベント内容</p>
+          <div class="busu_content">
+            <p class="summaryDetail_detail event"><span class="kigyou_text">企業ブース出展：60ブース</span></p>
+            <p class="summaryDetail_detail event">
+              <span class="bisines_text">ビジネス系セミナー：12枠</span>
+            </p>
+          </div>
+        </div>
+
+        <span class="border_dot busu"></span>
+
+        <div class="summaryDetail_content taishou">
+          <p class="summaryDetail_title taishou">対象者</p>
+          <p class="summaryDetail_detail keiei">
+            <span class="keiei_text">経営者<span class="syasen">/</span>フリーランス<span class="syasen">/</span>独立志望の会社員</span>
+          </p>
+        </div>
+
+        <span class="border_dot taishou"></span>
+
+        <div class="summaryDetail_content access">
+          <div class="summaryDetail_access_content">
+            <p class="summaryDetail_title access">アクセス</p>
+          </div>
+          <div class="moyori_content">
+            <p class="moyori_text">【最寄駅】</p>
+            <p class="mini_text title"><span class="dot"></span>JR</p>
+            <p class="mini_text eki">有楽町駅より徒歩1分</p>
+            <p class="mini_text eki">東京駅より徒歩5分</p>
+            <p class="mini_text title tika"><span class="dot"></span>地下鉄</p>
+            <p class="mini_text eki">有楽町線<span class="eki_text">：有楽町駅(B1F地下コンコースにて連絡)</span></p>
+            <p class="mini_text eki">千代田線<span class="eki_text">：二重橋前駅より徒歩5分/日比谷駅より徒歩7分</span></p>
+            <p class="mini_text eki">丸ノ内線<span class="eki_text">：銀座駅より徒歩5分</span></p>
+            <p class="mini_text eki">銀座線<span class="eki_text">：銀座駅より徒歩7分/京橋駅より徒歩7分</span></p>
+            <p class="mini_text eki">三田線<span class="eki_text">：日比谷駅より徒歩5分</span></p>
+
+          </div>
+          <div class="map_content">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3241.030113582723!2d139.76172800985458!3d35.67626050004339!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188bfabc68fde1%3A0xc4ca6a002ded263d!2z5p2x5Lqs5Zu96Zqb44OV44Kp44O844Op44Og!5e0!3m2!1sja!2sjp!4v1755060763615!5m2!1sja!2sjp" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+          </div>
+        </div>
+
+
+      </div>
     </div>
   </section>
 
